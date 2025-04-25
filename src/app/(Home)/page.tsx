21a0a12774c368e08c8d4cfae9a8167069d94058
spink994 <ayodeji.bakare994@gmail.com>
2025-04-25 15:15:13 +0100

    Scroll glitch

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -26,7 +26,7 @@ export default function LadningPage() {
     |--------------------------------------------------
     */
 	return (
-		<div className="flex h-screen isolate z-40 flex-col md:scroll-smooth md:snap-y md:snap-mandatory">
+		<div className="flex h-screen isolate z-40 flex-col md:scroll-smooth md:snap-y md:snap-mandatory max-w-screen overflow-x-clip">
 			{/**
             |--------------------------------------------------
             | Hero section
